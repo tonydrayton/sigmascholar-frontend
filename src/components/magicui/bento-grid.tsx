870b348f1cf54612e0a1fde2bb70595497cf9456
tony <tonydrayton43@gmail.com>
2025-03-02 10:18:21 -0500

    lint fix

--- src/components/magicui/bento-grid.tsx
+++ src/components/magicui/bento-grid.tsx
@@ -1,8 +1,7 @@
-import { ArrowRightIcon } from "@radix-ui/react-icons";
 import { ComponentPropsWithoutRef, ReactNode } from "react";
-
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { ArrowRightIcon } from "lucide-react";
 
 interface BentoGridProps extends ComponentPropsWithoutRef<"div"> {
   children: ReactNode;
